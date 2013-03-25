03afa13fbbeb4db3e7b1feb939c578f9db67a99d
Amir <amir.moravej@gmail.com>
2013-03-25 13:56:35 -0400

    Removing api wrapper (npm github). Switch to making https request directly against gitgub api.

--- server.js
+++ server.js
@@ -1,42 +1,19 @@
-var http = require('http');
-var GitHubApi = require("github");
+var https = require('https');
 var config = require('./config.js');
-var github = new GitHubApi({
-    version: "3.0.0",
-});
-github.authenticate({
-    type: "basic",
-    username: config.env.GITHUB.USERNAME,
-    password: config.env.GITHUB.PASSWORD
-});
-github.search.users({
-    keyword: "Canada"
-}, function(err, res) {
-    console.log(JSON.stringify(res));
-});
 
+var options = {
+  hostname: 'api.github.com', 
+  path: '/users/doobio',
+  method: 'GET',
+  username: config.env.GITHUB.USERNAME,
+  password: config.env.GITHUB.PASSWORD
+};
 
-
-// var options = {
-//   hostname: 'https://api.github.com', 
-//   port: 80,
-//   path: '/users/defunkt',
-//   method: 'GET'
-// };
-// var client_id = process.env.GITHUB_CLIENT_ID;
-// var client_secret = process.env.GITHUB_CLIENT_SECRET;
-// var server = http.createServer(function(req, res){
-// 	console.log(req.headers);
-// 	res.end('Thanks!');
-// }).listen(function(){
-// 	console.log('start listening on 1081')
-// },8080);
-
-// var req = http.request(options, function(res) {
-//   console.log('STATUS: ' + res.statusCode);
-//   console.log('HEADERS: ' + JSON.stringify(res.headers));
-//   res.setEncoding('utf8');
-//   res.on('data', function (chunk) {
-//     console.log('BODY: ' + chunk);
-//   });
-// });
+var req = https.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  // console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+}).end();
