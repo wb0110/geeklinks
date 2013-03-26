009f21382450a2754ff36b58ced7f984cf19d5dc
Amir <amir.moravej@gmail.com>
2013-03-26 10:13:33 -0400

    Adding nextFetch request and uuids function which fetch a uuid from chouchdb and calls the callback.

--- couch.js
+++ couch.js
@@ -1,26 +1,39 @@
 var http = require('http'),
-	couchNewDoc = {
+	couchNewDocRequest = {
 		hostname: '127.0.0.1',
 		port: 5984, 
 		method: 'PUT'
+	}, uuidRequest = {
+		hostname: '127.0.0.1',
+		port: 5984, 
+		method: 'GET'
 	};
 
+var uuids = function(callback) {
+	http.get('http://127.0.0.1:5984/_uuids', function(){
+		callback();
+	});
+};
+
 // Accepts a JSON array of 100 objects and add them the database.
-var addNewDoc = function(data) {
+var addNewDoc = function(data, next, callback) {
 	for (var i = 0; i < data.length; ++i) {
 		var doc = data[i], id = data[i]['id'];
-		couchNewDoc.path = '/test/' + id;
+		couchNewDocRequest.path = '/test/' + id;
 		doc = JSON.stringify(doc);
 		// console.log(id);
-		// var req = http.request(couchNewDoc, function(res){
-		// 	res.setEncoding('utf8');
-		// 	res.on('data', function(chunk){
-		// 		console.log(chunk);
-		// 		// Go back to the requestor and call for the next 100 objects. 
-		// 	});
-		// });
-		// req.write(doc);
-		// req.end();
+		var req = http.request(couchNewDocRequest, function(res){
+			res.setEncoding('utf8');
+			res.on('data', function(chunk){
+				console.log(chunk);
+				// Go back to the requestor and call for the next 100 objects. 
+			});
+			res.on('end', function(){
+				if (id == next) addNewTracker(next, callback);
+			});
+		});
+		req.write(doc);
+		req.end();
 	}
 };
 
