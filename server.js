b9b7481e0391ac5f31a8ea4fdff005bb6fbc7bec
Amir <amir.moravej@gmail.com>
2013-03-26 01:09:15 -0400

    Calling backend to store data.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var https = require('https'),
 	config = require('./config.js'),
+	couch = require('./couch.js'),
 	u = config.env.GITHUB.USERNAME,
 	p = config.env.GITHUB.PASSWORD;
 
 var options = {
 	hostname: 'api.github.com', 
 	method: 'GET',
+	path: '/repositories', 
+	// '/legacy/users/search/location?canada', 
+	//'/users/doobio/repos?type=owner',
 	// path: '/legacy/repos/search/canada',
-	path: '/repositories',
 	headers : {
 		'Authorization': 'Basic ' + new Buffer(u + ':' + p).toString('base64')
 	}
@@ -18,22 +21,15 @@ var req = https.request(options, function(res) {
 	console.log('STATUS: ' + res.statusCode);
 	console.log('X-RateLimit-Limit: ' + JSON.stringify(res.headers['x-ratelimit-limit']));
 	console.log('X-RateLimit-Remaining: ' + JSON.stringify(res.headers['x-ratelimit-remaining']));
-	console.log('HEADERS: ' + JSON.stringify(res.headers));
+	console.log('HEADERS: ');
+	next = findNext(res.headers['link']);
 	res.setEncoding('utf8');
 	res.on('data', function (chunk) {
 		data += chunk;
 	});
 	res.on('end', function(){
 		console.log('Response finished.');
-		var j = 0, javascript = 0, index = [];
 		data = JSON.parse(data);
-		for (var i in data) {
-			index.push(i);
-			++j;
-			if (data[i]['language'] == 'javascript') ++javascript;
-		}
-		console.log('Repository Count: %s', j);
-// console.log(data[99]);
-console.log('Javascript repositories: %s', javascript);
-});
+		couch.addNewDoc(data, addNext);
+	});
 }).end();
