8be9f7954956434b87d26b2718a40824291d4d58
Amir <amir.moravej@gmail.com>
2013-03-29 15:49:30 -0400

    Refinig mongo.find method.

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -1,26 +1,40 @@
 
 var mongo = (function(){
 	var mongodb = require("mongodb"),
-	    srv = new mongodb.Server('127.0.0.1', 27017);
+		srv = new mongodb.Server('127.0.0.1', 27017);
 
-	 var find = function(dbName, query, callback) {
-	 	if (!dbName) {
-	 		if (!callback) throw 'mongo.find: Invalid Database Name.';
-	 		else callback('mongo.find: Invalid Database Name.', null);
-	 		return;
-	 	}
-	 	if (!query) {
-	 		if (!callback)
-		 		throw 'mongo.find: Invalid Query.';
-		 	else callback('mongo.find: Invalid Query.', null);
-		 	return;
-	 	}
-	 	if (!callback) return;
+	var max = function(dbName, callback) {
+		if (!dbName) {
+			if (!callback) throw 'mongo.max: Invalid Database Name.';
+			else callback('mongo.max: Invalid Database Name.', null);
+			return;
+		} 
+		if (!callback) return;
+
+	}
+	var find = function(dbName, collectionName, query, callback) {
+		if (!dbName) {
+			if (!callback) throw 'mongo.find: Invalid Database Name.';
+			else callback('mongo.find: Invalid Database Name.', null);
+			return;
+		}
+		if (!collectionName) {
+			if (!callback) throw 'mongo.find: Invalid Collection Name.';
+			else callback('mongo.find: Collection Database Name.', null);
+			return;
+		}
+		if (!query) {
+			if (!callback)
+				throw 'mongo.find: Invalid Query.';
+			else callback('mongo.find: Invalid Query.', null);
+			return;
+		}
+		if (!callback) return;
 		connector.open(function(error, db){
 			var connector = new mongodb.Db(dbName, srv);
 			if (error) throw error;
 			console.log('Connected to: ' + dbName);
-			db.collection(dbName, function(error, collection){
+			db.collection(collectionName, function(error, collection){
 				if (error) throw error;
 				collection.find(query, function(error, cursor) {
 					if (error) {
@@ -31,10 +45,11 @@ var mongo = (function(){
 				});
 			});
 		});
-	 }
+	}
 
 	return {
-		find: find
+		find: find,
+		max: max
 	}
 }());
 
