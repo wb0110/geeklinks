bb5a34c023b32a59717c89059ce3feedf47083ef
Amir <amir.moravej@gmail.com>
2013-03-31 17:13:11 -0400

    Adding mongo.create().

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -62,10 +62,40 @@ var mongo = (function(){
 				});
 			});
 		});
+	};
+	var create = function(dbName, collectionName, docs, callback) {
+		srv = new mongodb.Server('127.0.0.1', 27017);
+		if (!dbName) {
+			if (!callback) throw 'mongo.find: Invalid Database Name.';
+			else return callback('mongo.find: Invalid Database Name.', null);
+		}
+		var connector = new mongodb.Db(dbName, srv);
+		if (!collectionName) {
+			if (!callback) throw 'mongo.find: Invalid Collection Name.';
+			else return callback('mongo.find: Invalid Collection Name.', null);
+		}
+		if (!docs) {
+			if (!callback)
+				throw 'mongo.find: Invalid documents to be inserted.';
+			else return callback('mongo.find: Invalid documents to be inserted.', null);
+		}
+		if (!callback) return;
+		connector.open(function(error, db){
+			assert.equal(error, null);
+			console.log('Connected to: ' + dbName);
+			db.collection(collectionName, function(error, col){
+				assert.equal(error, null);
+				db.createCollection(colName, function(err, collection){
+					for (var i in docs) collection.insert(docs[i]);
+					return callback();
+				});
+			});
+		});
 	}
 	return {
 		find: find,
-		max: max
+		max: max, 
+		create: create
 	}
 }());
 //////	TESTS.
