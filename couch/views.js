0ae2209286d8ad85fca8b53328f900cef1392e2f
Amir <amir.moravej@gmail.com>
2013-04-03 11:58:09 -0400

    Copyright info...

--- couch/views.js
+++ couch/views.js
@@ -1,10 +1,11 @@
-/*****************************************************************
-**************	Created by Amir Moravej	***************************
-**************	Copyrights (c) 2013 Amir Moravej.	***************
-**************	All rights reserved.	******************************
-**************	Contact: amir@doob.io *****************************
-******************************************************************/
-
+/*
+***********************************************
+**************	Created by Amir Moravej 
+**************	Copyrights (c) 2013 Amir Moravej
+**************	All rights reserved
+**************	Contact: amir@doob.io 
+***********************************************
+*/
 var couch = require('couch.js');
 // View the _stats of github_repos db.
 var repos_stats = (function(){
