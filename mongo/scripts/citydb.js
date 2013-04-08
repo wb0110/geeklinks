8bd9427a3b6d9667c48d534a8fc8c533d49a7891
Amir <amir.moravej@gmail.com>
2013-04-08 00:55:31 -0400

    Reading lines, assert != 10

--- mongo/scripts/citydb.js
+++ mongo/scripts/citydb.js
@@ -5,4 +5,26 @@
 **************	All rights reserved
 **************	Contact: amir@doob.io 
 ***********************************************
-*/
+*/ 
+var fs = require('fs'),
+	assert = require('assert'),
+	fpath = __dirname + '/' + 'cities.txt',
+	// Currently (April 2013) stability of ReadLine is: 2 -unstable.
+	readline = require('readline'), rd, flag = true, count = 0;
+
+fs.stat(fpath, function(err, stats){
+	assert.equal(err, null);
+	console.log(stats);
+});
+
+rd = readline.createInterface({
+	input: fs.createReadStream(fpath),
+	output: process.stdout,
+	terminal: false
+});
+
+rd.on('line', function(l){
+	console.log(l);
+	console.log(++count);
+	assert.notEqual(count, 10);
+});
